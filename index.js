abc4e158a513f16e04dbf571a7fe2c5e80ff8a97
Ruth <cirurue@gmail.com>
2025-04-01 21:56:55 +0300

    dom modifying elements lab done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+// remove the main node
+const mainNode = document.getElementById("main");
+if (mainNode) {
+  mainNode.remove();
+}
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Ruth is the champion";
+document.body.appendChild(newHeader);
